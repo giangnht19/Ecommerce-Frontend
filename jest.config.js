d2e43008401aee2c609a136aae5bae0d0ac0421d
giangnht19 <s223197048@deakin.edu.au>
2024-08-15 21:59:12 +1000

    delete frontend

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// jest.config.js
-module.exports = {
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest'
-    },
-    testEnvironment: 'node',
-    moduleFileExtensions: ['js', 'jsx', 'json', 'node']
-  };
-  
