5df7db4584d3af283cfeb874bd8a582979672444
giangnht19 <s223197048@deakin.edu.au>
2024-08-15 21:46:04 +1000

    fixed jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,9 @@
 // jest.config.js
 module.exports = {
     transform: {
-      '^.+\\.jsx?$': 'babel-jest' // Transforms .js and .jsx files using babel-jest
+      '^.+\\.jsx?$': 'babel-jest'
     },
-    testEnvironment: 'node', // Specify the test environment, such as Node.js
-    moduleFileExtensions: ['js', 'jsx', 'json', 'node'], // Include extensions your tests might use
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1' // Example alias configuration
-    }
+    testEnvironment: 'node',
+    moduleFileExtensions: ['js', 'jsx', 'json', 'node']
   };
   
